310c10bffcb66153e178f59540ab2dc81366d846
邝慧娜 <1228060928@qq.com>
2019-07-29 10:53:43 +0800

    test01

---
+++ Test01/src/main/java/Test01.java
@@ -0,0 +1,13 @@
+/**
+ * @author Lenovo
+ * @version v 1.0
+ * @date 2019/7/29
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("你好，世界");
+
+    }
+}
